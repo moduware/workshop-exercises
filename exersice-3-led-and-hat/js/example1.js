5e67cc93dbf57c6a6e27d9ff0f440af3144fd689
moemen.hegazy@gmail.com <moemen.hegazy@gmail.com>
2018-07-29 11:13:59 +0200

    fixing indentation

--- exersice-3-led-and-hat/js/example1.js
+++ exersice-3-led-and-hat/js/example1.js
@@ -10,77 +10,77 @@
  */
 
 document.addEventListener('DOMContentLoaded',
-	function(event) {
-		// Creating header on top of tile
-		Nexpaq.Header.create('Tile Template');
+  function(event) {
+    // Creating header on top of tile
+    Nexpaq.Header.create('Tile Template');
 
-	});
+  });
 
 document.addEventListener('WebViewApiReady',
-	function() {
+  function() {
 
-		var ledModuleUuid = '';
-		var hatModuleUuid = '';
-		var levelOne = 34;
-		var levelTwo = 38;
+    var ledModuleUuid = '';
+    var hatModuleUuid = '';
+    var levelOne = 34;
+    var levelTwo = 38;
 
-		// use GetCurrentConfiguration method to quickly get the information of
-		// the current connected modules.
-		Moduware.v0.API.GetCurrentConfiguration().then(function(result) {
+    // use GetCurrentConfiguration method to quickly get the information of
+    // the current connected modules.
+    Moduware.v0.API.GetCurrentConfiguration().then(function(result) {
 
-			ledModuleUuid = result[0].modules.find(function(module) {
-				return module !== null && module !== undefined && module.typeId === 'moduware.module.led';
-			}).uuid;
+      ledModuleUuid = result[0].modules.find(function(module) {
+        return module !== null && module !== undefined && module.typeId === 'moduware.module.led';
+      }).uuid;
 
-			hatModuleUuid = result[0].modules.find(function (module) {
-				return module !== null && module !== undefined && module.typeId === 'nexpaq.module.hat';
-			}).uuid;
+      hatModuleUuid = result[0].modules.find(function(module) {
+        return module !== null && module !== undefined && module.typeId === 'nexpaq.module.hat';
+      }).uuid;
 
-			//starting the the HAT sensor
-			Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StartSensor', []);
+      //starting the the HAT sensor
+      Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StartSensor', []);
 
-			// put flash on (white) to indicate that the HAT sensor is working
-			// what the first 2 parameters in the array means, interval and duration ??
-			Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StartFlashingRgbLeds', [65535, 65535, 255, 255, 255]);
-		});
+      // put flash on (white) to indicate that the HAT sensor is working
+      // what the first 2 parameters in the array means, interval and duration ??
+      Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StartFlashingRgbLeds', [65535, 65535, 255, 255, 255]);
+    });
 
-		// start listening to the DataReceived event
-		Moduware.v0.API.Module.addEventListener('DataReceived',
-			function(event) {
+    // start listening to the DataReceived event
+    Moduware.v0.API.Module.addEventListener('DataReceived',
+      function(event) {
 
-				// check if the event is triggered by the H&T module
-				if(event.moduleUuid !== hatModuleUuid) return;
+        // check if the event is triggered by the H&T module
+        if(event.moduleUuid !== hatModuleUuid) return;
 
-				// check if the event source is the sensor 
-				if(event.dataSource === 'SensorValue') {
+        // check if the event source is the sensor 
+        if(event.dataSource === 'SensorValue') {
 
-					var currentTemperature = parseFloat(event.variables.object_temperature);
-					console.log('current temperature: ' + currentTemperature);
+          var currentTemperature = parseFloat(event.variables.object_temperature);
+          console.log('current temperature: ' + currentTemperature);
 
-					// level one 
-					if(currentTemperature >= levelOne && currentTemperature < levelTwo) {
-						// set flash to blue (not danger)
-						Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [0, 153, 0]);
-					}
+          // level one 
+          if(currentTemperature >= levelOne && currentTemperature < levelTwo) {
+            // set flash to blue (not danger)
+            Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [0, 153, 0]);
+          }
 
-					// level two
-					if(currentTemperature >= levelTwo) {
-						// set flash to red (danger)
-						Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [255, 0, 0]);
-					}
+          // level two
+          if(currentTemperature >= levelTwo) {
+            // set flash to red (danger)
+            Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [255, 0, 0]);
+          }
 
-					// less than level one, means that we should go back to the non danger state (yellow)
-					if(currentTemperature < levelOne) {
-						Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [255, 255, 255]);
-					}
-				}
-			});
+          // less than level one, means that we should go back to the non danger state (yellow)
+          if(currentTemperature < levelOne) {
+            Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetRGB', [255, 255, 255]);
+          }
+        }
+      });
 
-		// clean up before exiting tile 
-		Moduware.v0.API.addEventListener('BeforeExit',
-			function() {
-				Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StopSensor', []);
-				Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StopFlashingRgbLeds', []);
-			});
+    // clean up before exiting tile 
+    Moduware.v0.API.addEventListener('BeforeExit',
+      function() {
+        Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StopSensor', []);
+        Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StopFlashingRgbLeds', []);
+      });
 
-	});
+  });
--- exersice-3-led-and-hat/js/example2.js
+++ exersice-3-led-and-hat/js/example2.js
@@ -6,87 +6,87 @@
  */
 
 document.addEventListener('DOMContentLoaded',
-	function(event) {
-		// Creating header on top of tile
-		Nexpaq.Header.create('Tile Template');
+  function(event) {
+    // Creating header on top of tile
+    Nexpaq.Header.create('Tile Template');
 
-	});
+  });
 
 document.addEventListener('WebViewApiReady',
-	function() {
+  function() {
 
-		var ledModuleUuid = '';
-		var hatModuleUuid = '';
-		var currentBrightness = 100;
-		var factor = 300;
-		var previousTemperature = 0;
+    var ledModuleUuid = '';
+    var hatModuleUuid = '';
+    var currentBrightness = 100;
+    var factor = 300;
+    var previousTemperature = 0;
 
-		// use GetCurrentConfiguration method to quickly get the information of
-		// the current connected modules.
-		Moduware.v0.API.GetCurrentConfiguration().then(function(result) {
-			ledModuleUuid = result[0].modules[0].uuid;
-			hatModuleUuid = result[0].modules[1].uuid;
+    // use GetCurrentConfiguration method to quickly get the information of
+    // the current connected modules.
+    Moduware.v0.API.GetCurrentConfiguration().then(function(result) {
+      ledModuleUuid = result[0].modules[0].uuid;
+      hatModuleUuid = result[0].modules[1].uuid;
 
-			//starting the the HAT sensor
-			Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StartSensor', []);
+      //starting the the HAT sensor
+      Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StartSensor', []);
 
-			// put flash on (leds) to indicate that the HAT sensor is working
-			Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StartFlashingFlashLeds', [65535, 65535, currentBrightness]);
-		});
+      // put flash on (leds) to indicate that the HAT sensor is working
+      Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StartFlashingFlashLeds', [65535, 65535, currentBrightness]);
+    });
 
-		// start listening to the DataReceived event
-		Moduware.v0.API.Module.addEventListener('DataReceived',
-			function(event) {
+    // start listening to the DataReceived event
+    Moduware.v0.API.Module.addEventListener('DataReceived',
+      function(event) {
 
-				// check if the event is triggered by the H&T module
-				if(event.moduleUuid !== hatModuleUuid) return;
+        // check if the event is triggered by the H&T module
+        if(event.moduleUuid !== hatModuleUuid) return;
 
-				// check if the event source is the sensor 
-				if(event.dataSource === 'SensorValue') {
+        // check if the event source is the sensor 
+        if(event.dataSource === 'SensorValue') {
 
-					// parse the value of object temperature to number
-					var currentTemperature = parseFloat(event.variables.object_temperature);
+          // parse the value of object temperature to number
+          var currentTemperature = parseFloat(event.variables.object_temperature);
 
-					// set the prev temp to current temp only if the prev temp is zero 
-					// which indicates first time run
-					if(previousTemperature === 0) {
-						previousTemperature = currentTemperature;
-					}
+          // set the prev temp to current temp only if the prev temp is zero 
+          // which indicates first time run
+          if(previousTemperature === 0) {
+            previousTemperature = currentTemperature;
+          }
 
-					// temperature increases
-					if(currentTemperature > previousTemperature) {
+          // temperature increases
+          if(currentTemperature > previousTemperature) {
 
-						currentBrightness = currentBrightness + Math.ceil((currentTemperature - previousTemperature) * factor);
+            currentBrightness = currentBrightness + Math.ceil((currentTemperature - previousTemperature) * factor);
 
-						// increase brigthness
-						if(currentBrightness < 9000) {
-							Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetFlashes', [currentBrightness, currentBrightness]);
-						}
-					}
+            // increase brigthness
+            if(currentBrightness < 9000) {
+              Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetFlashes', [currentBrightness, currentBrightness]);
+            }
+          }
 
-					// temperature decreases
-					if(currentTemperature < previousTemperature) {
+          // temperature decreases
+          if(currentTemperature < previousTemperature) {
 
-						currentBrightness = currentBrightness - Math.ceil((previousTemperature - currentTemperature) * factor);
+            currentBrightness = currentBrightness - Math.ceil((previousTemperature - currentTemperature) * factor);
 
-						if(currentBrightness <= 0) {
-							currentBrightness = 100;
-						}
+            if(currentBrightness <= 0) {
+              currentBrightness = 100;
+            }
 
-						// decrease brightness
-						Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetFlashes', [currentBrightness, currentBrightness]);
-					}
+            // decrease brightness
+            Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'SetFlashes', [currentBrightness, currentBrightness]);
+          }
 
-					previousTemperature = currentTemperature;
-					console.log('current temperature: ' + currentTemperature);
-					console.log('current brightness: ' + currentBrightness);
-				}
-			});
+          previousTemperature = currentTemperature;
+          console.log('current temperature: ' + currentTemperature);
+          console.log('current brightness: ' + currentBrightness);
+        }
+      });
 
-		// clean up before exit tile 
-		Moduware.v0.API.addEventListener('BeforeExit',
-			function() {
-				Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StopSensor', []);
-				Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StopFlashingFlashLeds', []);
-			});
-	});
+    // clean up before exit tile 
+    Moduware.v0.API.addEventListener('BeforeExit',
+      function() {
+        Moduware.v0.API.Module.SendCommand(hatModuleUuid, 'StopSensor', []);
+        Moduware.v0.API.Module.SendCommand(ledModuleUuid, 'StopFlashingFlashLeds', []);
+      });
+  });
