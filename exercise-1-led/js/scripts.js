ac243c9034e4c0bad768ddbb728124d9f81abdee
Alex Chernov <alexusblack@gmail.com>
2018-08-08 12:56:44 +1000

    complete exercise code

--- exercise-1-led/js/scripts.js
+++ exercise-1-led/js/scripts.js
@@ -13,11 +13,32 @@ document.addEventListener('WebViewApiReady', function() {
   Moduware.v0.API.Module.addEventListener('DataReceived', dataReceivedHandler);
 
   // this will turn on the light in the LED Module once the tile is loaded as an indication that it work
-  Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [255, 255, 255]);
+  // Original: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [255, 255, 255]);
+  // Red: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [255, 0, 0]);
+  // Blue: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [0, 255, 0]);
+  // Green: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [0, 0, 255]);
+  // Flash LEDs: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetFlashes', [2000, 2000]);
+  // Specific LEDs: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetOneLEDInRGB', [0b00010101, 150, 150, 150]);
+
+  // Major:
+  var numberOfLedsOn = 0;
+  document.getElementById('toggleLightButton').addEventListener('click', function() {
+    numberOfLedsOn = numberOfLedsOn + 1;
+    if(numberOfLedsOn > 6) {
+      numberOfLedsOn = 0;
+      Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetOneLEDInRGB', [0b00111111, 0, 0, 0]);
+    } else {
+      Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetOneLEDInRGB', [Math.pow(2, numberOfLedsOn) - 1, 30, 30, 30]);
+    }
+  });
+  
 
   Moduware.v0.API.addEventListener('BeforeExit', function() {
     // do your clean up code here, like switching off sensors or light
-    Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'TurnOffLeds', []);
+    // Original, Red, Blue, Green: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'TurnOffLeds', []);
+    // Flash LEDs: // Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'TurnOffFlashs', []);
+    // Specific LEDs & Major:
+    Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetOneLEDInRGB', [0b00111111, 0, 0, 0]);
   });
 
 });
